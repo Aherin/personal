df2be751eb8d885475133bb2b16525fce7c4f0c9
Cristian Giraldo <cristian.giraldo@live.com>
2021-11-04 12:05:51 -0500

    Se agrega el logo

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import logo from '../../assets/logo/logo.png';
 
 export const NavBar = () => {
 
@@ -7,30 +8,32 @@ export const NavBar = () => {
     return (
         <>
             <section className="navbar section animate__animated animate__fadeInDown animate__slow" role="navigation">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="https://bulma.io">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="" />
-                    </a>
-                    <div className="navbar-item">
-                        <p className="subtitle"><strong>Cristian Giraldo</strong></p>
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="https://bulma.io">
+                            <img src={logo} alt="" />
+                        </a>
+                        <div className="navbar-item">
+                            <p className="subtitle"><strong></strong></p>
+                        </div>
+                        <button className={`navbar-burger ${isActive ? "is-active" : ""}`} data-target="navbarBasicExample" onClick={() => setisActive(!isActive)}>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </button>
                     </div>
-                    <button className={`navbar-burger ${isActive ? "is-active" : ""}`} data-target="navbarBasicExample" onClick={() => setisActive(!isActive)}>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-                <div id="navbarBasicExample" className={`navbar-menu animate__animated animate__fadeIn ${isActive ? "is-active" : ""}`}>
-                    <div className="navbar-end">
-                        <a className="navbar-item" href="/">
-                            <strong>About</strong>
-                        </a>
-                        <a className="navbar-item" href="/">
-                            <strong>Contact</strong>
-                        </a>
-                        <a className="navbar-item" href="/">
-                            <div className="button is-dark"><strong>View Work</strong></div>
-                        </a>
+                    <div id="navbarBasicExample" className={`navbar-menu animate__animated animate__fadeIn ${isActive ? "is-active" : ""}`}>
+                        <div className="navbar-end">
+                            <a className="navbar-item" href="/">
+                                <strong>About</strong>
+                            </a>
+                            <a className="navbar-item" href="/">
+                                <strong>Contact</strong>
+                            </a>
+                            <a className="navbar-item" href="/">
+                                <div className="button is-dark"><strong>View Work</strong></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
