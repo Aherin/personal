672219f232bf2198ef46838e69e246d0f34b27cb
Cristian Giraldo <cristian.giraldo@live.com>
2021-11-04 23:04:17 -0500

    Added despcriptions

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/logo.png';
+import curriculum from '../../assets/curriculum/CristianGiraldo2021CV-V2.pdf';
 
 export const NavBar = () => {
 
@@ -10,29 +12,29 @@ export const NavBar = () => {
             <section className="navbar section animate__animated animate__fadeInDown animate__slow" role="navigation">
                 <div className="container">
                     <div className="navbar-brand">
-                        <a className="navbar-item" href="https://bulma.io">
+                        <Link className="navbar-item" to="/personal">
                             <img src={logo} alt="" />
-                        </a>
+                        </Link>
                         <div className="navbar-item">
                             <p className="subtitle"><strong></strong></p>
                         </div>
-                        <button className={`navbar-burger ${isActive ? "is-active" : ""}`} data-target="navbarBasicExample" onClick={() => setisActive(!isActive)}>
+                        <button className={`navbar-burger ${ isActive ? "is-active" : "" }` } data-target="navbarBasicExample" onClick={ () => setisActive(!isActive) }>
                             <span aria-hidden="true"></span>
                             <span aria-hidden="true"></span>
                             <span aria-hidden="true"></span>
                         </button>
                     </div>
-                    <div id="navbarBasicExample" className={`navbar-menu animate__animated animate__fadeIn ${isActive ? "is-active" : ""}`}>
+                    <div id="navbarBasicExample" className={ `navbar-menu animate__animated animate__fadeIn ${ isActive ? "is-active" : "" }` }>
                         <div className="navbar-end">
-                            <a className="navbar-item" href="/">
+                            <a className="navbar-item" href="/personal">
                                 <strong>About</strong>
                             </a>
-                            <a className="navbar-item" href="/">
+                            <a className="navbar-item" href="https://www.linkedin.com/in/giraldo-cristian/" target="_blank" rel="noreferrer">
                                 <strong>LinkedIn</strong>
                             </a>
-                            <a className="navbar-item" href="/">
+                            <Link className="navbar-item" to={ curriculum } target="_blank">
                                 <div className="button is-dark"><strong>Curriculum</strong></div>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -10,19 +10,19 @@ export const LandingPage = () => {
                 <div className="hero-head">
                     <NavBar />
                 </div>
-                <div className="hero-body animate__animated animate__fadeIn animate__slow">
+                <div className="hero-body">
                     <div className="container">
                         <div className="columns">
                             <div className="column is-one-quarter px-6 pb-6">
-                                <figure className="image ">
-                                    <img className="rounded-corners" src={profile} alt="" />
+                                <figure className="image " data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
+                                    <img className="rounded-corners" src={ profile } alt="" />
                                 </figure>
                             </div>
-                            <div className="column px-6 has-text-left-desktop-only has-text-centered-mobile">
-                                <p className="title is-size-1 mb-6">
+                            <div className="column px-6 has-text-centered-mobile has-text-left-desktop-only">
+                                <p className="title is-size-1 mb-6" data-aos="fade-right" data-aos-duration="1000">
                                     Hi There!
                                 </p>
-                                <p className="subtitle is-size-3">
+                                <p className="subtitle is-size-3" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
                                     Fuelled by a passion for designing compelling products, I have a deep desire to excel and
                                     continuously improve in my work. Learn more about my journey below
                                 </p>
@@ -39,28 +39,31 @@ export const LandingPage = () => {
                             <p class="title is-size-1 mb-6 " data-aos="fade-right" data-aos-duration="1000">
                                 My Career So Far
                             </p>
-                            <p class="subtitle is-size-3" data-aos="fade-right" data-aos-duration="1000">
-                                Always up for a challenge, I have worked for lean start-ups and was a member of the first
-                                New Zealand start-up to attend Y Combinator, the largest start-up accelerator in the
-                                world. From there, I worked my way up to Art Director and Team Lead at Appster where I
-                                oversaw the design of 30+ mobile and desktop apps. Currently, I lead UI/UX design at SaaS
-                                start-up VideoMyJob.
+                            <p class="subtitle is-size-3" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
+                                Skilled Software Developer with a Bachelor of Science degree in Electronic
+                                Engineering and more than 5 years of programming experience. Qualified to
+                                work with Java Application and a history of working in the information
+                                technology and services industry. Experienced in Frontend development with
+                                Angular and React.
                             </p>
                         </div>
                         <div class="column px-6 ">
                             <div class="tags are-large is-flex is-justify-content-center" >
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">UI DESIGN</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">UX DESIGN</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">PROTOTYPING</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">BRANDING</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">HTML/CSS</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">WIREFRAMING</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">INFORMATION ARCHITECTURE</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">USER RESEARCH</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">USER INTERVIEWS</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">LEADERSHIP</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">SKETCH</span>
-                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">ADOBE SUITE</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">SPRING BOOT</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">ELASTICSEARCH</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">MICROPROFILE</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">DATABASES</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">JAVASCRIPT</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">QUARKUS</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">ANGULAR</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">NODEJS</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">REACT</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">DOCKER</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">JUNIT</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">JAVA</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">JEST</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">CSS</span>
+                                <span class="tag is-rounded is-dark" data-aos="flip-right" data-aos-duration="1000">HTML</span>
                             </div>
                         </div>
                     </div>
